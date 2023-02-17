e7c1f96322b12871ad1979c6a2a0e4f84a583bb4
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-02-17 16:04:51 +0000

    Update FrontEnd.md

--- src/application/slack-adapter/conversation-event-handler.ts
+++ src/application/slack-adapter/conversation-event-handler.ts
@@ -7,4 +7,5 @@ import {
   ConversationStarted,
 } from "../../domain/conversation/conversation.events";
 import { SlackConversationView } from "../../domain/slack-adapter/slack-adapter.dto";
-import { SlackConversationDynamodbRepository } from "../../infrastructure
+import { SlackConversationDynamodbRepository } from "../../infrastructure/dynamodb/slack/slack-conversation-dynamodb.repository";
+import { SlackMessageHelpers } from "../../infrastructure/slack/sl
