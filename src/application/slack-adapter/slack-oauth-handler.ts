1f94c4afe97658fd8bfd0b0de2d17f64145dca4f
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-03-10 19:13:05 +0000

    Updated Travis config.

--- src/application/slack-adapter/slack-oauth-handler.ts
+++ src/application/slack-adapter/slack-oauth-handler.ts
@@ -1,3 +1,4 @@
 import type * as Lambda from "aws-lambda";
 import config from "../../config";
-import { SlackOAuthReaderRepository } from "../../infrastructure/dynamodb/slack/oauth/slack
+import { SlackOAuthReaderRepository } from "../../infrastructure/dynamodb/slack/oauth/slack-oauth-reader.repository";
+import { SlackOAuthWriterRepository } from "../../infrastructure/dynamodb/slack/oauth/slack-o
