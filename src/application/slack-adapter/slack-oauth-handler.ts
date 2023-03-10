5bf24f7f01cd995760228e2d90cfb0f037822071
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-03-10 15:18:21 +0000

    Changed Dockerfile

--- src/application/slack-adapter/slack-oauth-handler.ts
+++ src/application/slack-adapter/slack-oauth-handler.ts
@@ -1,2 +1,3 @@
 import type * as Lambda from "aws-lambda";
-import config from "../../conf
+import config from "../../config";
+import { SlackOAuthReaderRepository } from "../../infrastructure/dynamodb/slack/oauth/slack
