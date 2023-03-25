ca6c0debbac973ec7ed30af91c346d94a9e2bb2e
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-03-25 06:25:05 +0000

    Fixed conflicts

--- src/domain/bus/event-bus.ts
+++ src/domain/bus/event-bus.ts
@@ -1,1 +1,5 @@
-import { SendMessageCommand, SQSClient } fro
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+import { defaultSQSClient } from "./sqs";
+import { getEnv } from "../../env";
+
+export type DomainEvent
