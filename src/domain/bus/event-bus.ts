f9c609f1d28b22cd423c96724a0af0b798908e03
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-03-25 20:37:53 +0000

    * tests

--- src/domain/bus/event-bus.ts
+++ src/domain/bus/event-bus.ts
@@ -2,4 +2,7 @@ import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
 import { defaultSQSClient } from "./sqs";
 import { getEnv } from "../../env";
 
-export type DomainEvent
+export type DomainEvent = any & { type: string; conversationId: string };
+
+export class EventBus {
+  private static readonly QUEUE_URL = getEnv(
