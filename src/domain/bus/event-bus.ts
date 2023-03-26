b37b1e2fccabf0479710f98fa9ab69eb213d3df1
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-03-26 11:14:18 +0000

    remove package-lock

--- src/domain/bus/event-bus.ts
+++ src/domain/bus/event-bus.ts
@@ -5,4 +5,8 @@ import { getEnv } from "../../env";
 export type DomainEvent = any & { type: string; conversationId: string };
 
 export class EventBus {
-  private static readonly QUEUE_URL = getEnv(
+  private static readonly QUEUE_URL = getEnv("EVENT_BUS_SQS");
+
+  constructor(private readonly sqsClient: SQSClient = defaultSQSClient) {}
+
+  async publish(event: DomainEvent)
