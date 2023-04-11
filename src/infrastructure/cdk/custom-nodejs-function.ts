265d4f4ce77c1be04953edda16047c02e65c14eb
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-04-11 08:14:02 +0000

    Fix import ordering

---
+++ src/infrastructure/cdk/custom-nodejs-function.ts
@@ -0,0 +1,32 @@
+
+import { Architecture, Runtime } from "aws-cdk-lib/aws-lambda";
+import {
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import { EnvKey } from "../../env";
+
+export type CustomNodejsFunctionProps = Pick<
+  NodejsFunctionProps,
+  "entry" | "description" | "timeout"
+> & { environment: Record<EnvKey, string> };
+
+export class CustomNodejsFunction extends NodejsFunction {
+  constructor(scope: Construct, id: string, props: CustomNodejsFunctionProps) {
+    super(scope, id, {
+      ...props,
+      runtime: Runtime.NODEJS_18_X,
+      architecture: Architecture.ARM_64,
+      awsSdkConnectionReuse: true,
+      environment: {
+        ...props.environment,
+        NODE_OPTIONS: "--enable-source-maps",
+      },
+      bundling: {
+        sourceMap: true,
+        externalModules: ["@aws-sdk/*", "@aws-cdk/*"],
+      },
+    });
+  }
+}
