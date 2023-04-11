97bb82b3b57324f6395563ad2ba0a241f5395e1e
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-04-11 16:19:12 +0000

    update rule

--- src/infrastructure/cdk/main.ts
+++ src/infrastructure/cdk/main.ts
@@ -3,4 +3,8 @@ import * as apigwv2 from "@aws-cdk/aws-apigatewayv2-alpha";
 import {
   App,
   aws_lambda_event_sources as LambdaEventSources,
- 
+  Duration,
+  Stack,
+  StackProps,
+} from "aws-cdk-lib";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
