309a25f2987fa50d9e736c12fb43d1a028429ecf
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-04-22 16:41:03 +0000

    .cvsignore

--- src/infrastructure/cdk/slack-resources.ts
+++ src/infrastructure/cdk/slack-resources.ts
@@ -36,3 +36,138 @@ type SlackResourcesProps = {
 };
 
 export class SlackResources extends Construct {
+  private static readonly SLACK_OAUTH_CALLBACK_PATH = "/slack/oauth/callback";
+  private static readonly SLACK_EVENTS_PATH = "/slack/events";
+
+  protected readonly pSlackAdapterLambda: CustomNodejsFunction;
+  protected readonly pSlackAdapterLambdaAlias: Alias;
+
+  constructor(scope: Construct, props: SlackResourcesProps) {
+    super(scope, "SlackResources");
+
+    const slackEventBus = new SlackEventBus(this, "SlackEventBus", {
+      secret: props.secret,
+      httpApi: props.httpApi,
+      singleApp: {
+        appId: props.appId,
+        eventsApiPath: SlackResources.SLACK_EVENTS_PATH,
+      },
+    });
+
+    const slackConversationViewTable = new Table(
+      this,
+      "SlackConversationViewTable",
+      {
+        partitionKey: { name: "threadId", type: AttributeType.STRING },
+        billingMode: BillingMode.PAY_PER_REQUEST,
+      }
+    );
+
+    slackConversationViewTable.addGlobalSecondaryIndex({
+      indexName: CONVERSATION_ID_INDEX_NAME,
+      projectionType: ProjectionType.ALL,
+      partitionKey: { name: "conversationId", type: AttributeType.STRING },
+    });
+
+    this.pSlackAdapterLambda = new CustomNodejsFunction(
+      this,
+      "SlackAdapterLambda",
+      {
+        entry: resolve(__dirname, "../lambdas/slack-adapter.lambda.ts"),
+        description:
+          "Listens and processes Slack Events, Slack oAuth calls and Conversation API events",
+        timeout: SLACK_ADAPTER_LAMBDA_TIMEOUT,
+        environment: {
+          SLACK_SECRET_ARN: props.secret.secretArn,
+          COMMAND_BUS_SQS: props.conversationCommandSQS.queueUrl,
+          DYNAMODB_TABLE_SLACK_CONVERSATION_VIEW:
+            slackConversationViewTable.tableName,
+          DYNAMODB_INDEX_VIEW_CONVERSATION_ID: CONVERSATION_ID_INDEX_NAME,
+        } as Record<EnvKey, string>,
+      }
+    );
+
+    this.pSlackAdapterLambdaAlias = new Alias(
+      this,
+      "SlackHandlerLambdaLiveAlias",
+      {
+        aliasName: "latest",
+        version: this.pSlackAdapterLambda.currentVersion,
+      }
+    );
+
+    // slack adapter permissions
+    props.secret.grantRead(this.pSlackAdapterLambdaAlias);
+    slackConversationViewTable.grantReadWriteData(
+      this.pSlackAdapterLambdaAlias
+    );
+    props.conversationCommandSQS.grantSendMessages(
+      this.pSlackAdapterLambdaAlias
+    );
+
+    // bind slack adapter to queues
+    new Events.Rule(this, "SlackEventRule", {
+      enabled: true,
+      eventPattern: {
+        source: ["com.slack"],
+        detailType: ["EventCallback.message"],
+      },
+      targets: [
+        new EventsTargets.LambdaFunction(this.pSlackAdapterLambdaAlias),
+      ],
+      eventBus: slackEventBus.eventBus,
+    });
+
+    this.pSlackAdapterLambdaAlias.addEventSource(
+      new LambdaEventSources.SqsEventSource(props.conversationEventSQS, {
+        enabled: true,
+        batchSize: 1,
+      })
+    );
+
+    this.addOAuthResourcesIfNecessary(props);
+
+    new CfnOutput(this, "SlackEventRequestUrl", {
+      value: slackEventBus.slackEventsRequestUrl(props.appId),
+      description: "Slack Events Request Url to use in Slack API Dashboard",
+    });
+  }
+
+  private addOAuthResourcesIfNecessary(props: SlackResourcesProps) {
+    if (props.authType === "TOKEN_BASED") {
+      this.pSlackAdapterLambda.addEnvironment(
+        "OAUTH_TABLE_NAME",
+        "not-configured-for-oAuth"
+      );
+
+      return;
+    }
+
+    const oAuthTable = new Table(this, "oAuthTable", {
+      partitionKey: { name: "PK", type: AttributeType.STRING },
+      sortKey: { name: "SK", type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
+
+    oAuthTable.grantReadWriteData(this.pSlackAdapterLambdaAlias);
+
+    this.pSlackAdapterLambda.addEnvironment(
+      "OAUTH_TABLE_NAME",
+      oAuthTable.tableName
+    );
+
+    // add for handling slack oauth callbacks
+    props.httpApi.addRoutes({
+      path: SlackResources.SLACK_OAUTH_CALLBACK_PATH,
+      methods: [apigwv2.HttpMethod.GET],
+      integration: new HttpLambdaIntegration(
+        "OAuthCallbackIntegration",
+        this.pSlackAdapterLambdaAlias
+      ),
+    });
+
+    new CfnOutput(this, "SlackOAuthCallbackUrl", {
+      value: `${props.httpApi.apiEndpoint}${SlackResources.SLACK_OAUTH_CALLBACK_PATH}`,
+      description: "Slack Events Request Url to use in Slack API Dashboard",
+    });
+  }
