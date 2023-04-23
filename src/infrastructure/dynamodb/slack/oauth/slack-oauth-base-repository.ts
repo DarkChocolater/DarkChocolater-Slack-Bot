aaf9633c51a0a9a2a4150ae96846aa85836bc8cf
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-04-23 23:04:18 +0000

    new doc

--- src/infrastructure/dynamodb/slack/oauth/slack-oauth-base-repository.ts
+++ src/infrastructure/dynamodb/slack/oauth/slack-oauth-base-repository.ts
@@ -3,3 +3,6 @@ import { getEnv } from "../../../../env";
 import { defaultDynamoDBDocumentClient } from "../../crud/dynamodb-clients";
 
 export abstract class SlackOAuthBaseRepository {
+  protected static readonly TABLE_NAME = getEnv("OAUTH_TABLE_NAME");
+  protected static readonly PK_FIELD = "PK";
+  protected sta
