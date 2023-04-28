005d07e1f70ecdad526c00044ed41f2b1ce8ebce
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-04-28 19:17:03 +0000

    flake8

--- src/infrastructure/dynamodb/slack/oauth/slack-oauth-reader.repository.ts
+++ src/infrastructure/dynamodb/slack/oauth/slack-oauth-reader.repository.ts
@@ -1,1 +1,3 @@
-import { GetCommand } from "@aws-sdk/lib-d
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
+import { SlackOAuthBaseRepository } from "./slack-oauth-base-repository";
+import { TeamAccess, UserAcce
