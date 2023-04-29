79568bbf377a0ae8d26c288a2fc40249f7c788e0
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-04-29 20:13:33 +0000

    Fix windows build.

--- src/infrastructure/dynamodb/slack/oauth/slack-oauth-reader.repository.ts
+++ src/infrastructure/dynamodb/slack/oauth/slack-oauth-reader.repository.ts
@@ -1,3 +1,6 @@
 import { GetCommand } from "@aws-sdk/lib-dynamodb";
 import { SlackOAuthBaseRepository } from "./slack-oauth-base-repository";
-import { TeamAccess, UserAcce
+import { TeamAccess, UserAccess } from "../../../slack/oauth/oauth.dto";
+
+export class SlackOAuthReaderRepository extends SlackOAuthBaseRepository {
+  constructor() {
