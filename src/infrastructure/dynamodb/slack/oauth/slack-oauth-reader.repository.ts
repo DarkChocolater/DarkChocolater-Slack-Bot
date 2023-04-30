46c7b6c004ce329dc0d093e3eaff2430a99c416b
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-04-30 07:04:48 +0000

    Updated spirv-tools.

--- src/infrastructure/dynamodb/slack/oauth/slack-oauth-reader.repository.ts
+++ src/infrastructure/dynamodb/slack/oauth/slack-oauth-reader.repository.ts
@@ -4,3 +4,11 @@ import { TeamAccess, UserAccess } from "../../../slack/oauth/oauth.dto";
 
 export class SlackOAuthReaderRepository extends SlackOAuthBaseRepository {
   constructor() {
+    super();
+  }
+
+  async getTeamAccess(
+    appId: string,
+    teamId: string
+  ): Promise<TeamAccess | undefined> {
+    const 
