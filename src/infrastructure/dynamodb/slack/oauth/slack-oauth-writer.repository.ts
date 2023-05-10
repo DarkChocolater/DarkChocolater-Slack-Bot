37930ebe05594d17d9c53539bd3976cf93029a57
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-05-10 18:28:47 +0000

    Correct imports

--- src/infrastructure/dynamodb/slack/oauth/slack-oauth-writer.repository.ts
+++ src/infrastructure/dynamodb/slack/oauth/slack-oauth-writer.repository.ts
@@ -2,4 +2,12 @@ import { PutCommand } from "@aws-sdk/lib-dynamodb";
 import { SlackOAuthBaseRepository } from "./slack-oauth-base-repository";
 import { TeamAccess, UserAccess } from "../../../slack/oauth/oauth.dto";
 
-export class SlackOAuthWriterRepository extends SlackOAut
+export class SlackOAuthWriterRepository extends SlackOAuthBaseRepository {
+  constructor() {
+    super();
+  }
+
+  async putTeamAccess(
+    appId: string,
+    teamId: string,
+    team
