624de2b0d7158372dfdeea50b8bb836f8909d669
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-05-10 16:55:06 +0000

    fixes #31

--- src/infrastructure/dynamodb/slack/oauth/slack-oauth-writer.repository.ts
+++ src/infrastructure/dynamodb/slack/oauth/slack-oauth-writer.repository.ts
@@ -1,3 +1,5 @@
 import { PutCommand } from "@aws-sdk/lib-dynamodb";
 import { SlackOAuthBaseRepository } from "./slack-oauth-base-repository";
-im
+import { TeamAccess, UserAccess } from "../../../slack/oauth/oauth.dto";
+
+export class SlackOAuthWriterRepository extends SlackOAut
