b4b038373182573712cda3ecb600fb12a2e5d900
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-06-01 09:09:13 +0000

    Add CI configuration

--- src/infrastructure/lambdas/conversation.lambda.ts
+++ src/infrastructure/lambdas/conversation.lambda.ts
@@ -2,4 +2,6 @@ import { createHandler, EventListenerLambda, SQSEvent } from "./lambda";
 import { ConversationCommandHandler } from "../../application/conversation/conversation-command.handler";
 import { ConversationCommand } from "../../domain/conversation/conversation.commands";
 
-class Conv
+class ConversationLambda extends EventListenerLambda {
+  constructor(
+    private readonly conversationCommandHandler = new Convers
