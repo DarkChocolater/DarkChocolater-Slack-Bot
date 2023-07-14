818211408550c6bfad2fd08902abf2e93cd62d03
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-07-14 20:07:57 +0000

    Update archive

--- src/infrastructure/lambdas/open-ai.lambda.ts
+++ src/infrastructure/lambdas/open-ai.lambda.ts
@@ -1,3 +1,7 @@
 import { AsyncLambda, createHandler } from "./lambda";
 import { OpenAICommandHandler } from "../../application/open-ai/open-ai-command.handler";
-import { ConversationAICommand } from "../../domain/c
+import { ConversationAICommand } from "../../domain/conversation/ai/conversation-ai.commands";
+
+class OpenAILambda extends AsyncLambda<ConversationAICommand> {
+  constructor(
+    private rea
