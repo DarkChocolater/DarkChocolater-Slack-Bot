cea33cf6f90b6383ae5d3ca9bde7ed5c69825a90
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-07-20 16:13:11 +0000

    4.12.0

--- src/infrastructure/lambdas/slack-adapter.lambda.ts
+++ src/infrastructure/lambdas/slack-adapter.lambda.ts
@@ -8,4 +8,6 @@ import {
 } from "./lambda";
 import { ConversationEventHandler } from "../../application/slack-adapter/conversation-event-handler";
 import { SlackEventHandler } from "../../application/slack-adapter/slack-event-handler";
-import { SlackOAuthHandler } from "../../a
+import { SlackOAuthHandler } from "../../application/slack-adapter/slack-oauth-handler";
+import { DomainEvent } from "../../domain/bus/event-bus";
+import { SlackEventType } from ".
