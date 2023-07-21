fd42aa332efaca265a6647bd0537c324676292ac
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-07-21 22:57:04 +0000

    Fix summary

--- src/infrastructure/lambdas/slack-adapter.lambda.ts
+++ src/infrastructure/lambdas/slack-adapter.lambda.ts
@@ -13,3 +13,6 @@ import { DomainEvent } from "../../domain/bus/event-bus";
 import { SlackEventType } from "../../domain/slack-adapter/slack-adapter.dto";
 
 class SlackAdapterLambda extends EventListenerLambda<SlackEventBridgeEvent> {
+  constructor(
+    private readonly slackEventHandler: SlackEventHandler = new SlackEventHandler(),
+    private readonly
