619803acc4dc4ae7d8609352fe5f19a839c86f43
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-08-11 15:17:55 +0000

    Import cleanup

--- src/infrastructure/lambdas/tools/index.ts
+++ src/infrastructure/lambdas/tools/index.ts
@@ -14,4 +14,9 @@ export class Tools {
    * `Index.getInstance` only supposed to be used in Lambda deployments
    * using it in other types of developments could cause unintended effects
    * like request contexts getting mixed when multiple requests are processed
-   * by the same application i
+   * by the same application instance asynchronously
+   */
+  static getInstance(): Tools {
+    if (this.instance === undefined) {
+      throw new Error(
+   
