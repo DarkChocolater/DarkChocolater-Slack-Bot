74a0d8445002a95b8e529311241b183e503fe2df
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-08-18 17:33:08 +0000

    Fixed issue #11

--- src/infrastructure/openai/conversation-prompt-service.factory.ts
+++ src/infrastructure/openai/conversation-prompt-service.factory.ts
@@ -1,3 +1,7 @@
 import { ConversationPromptService } from "@wisegpt/gpt-conversation-prompt";
 import { Configuration, OpenAIApi } from "openai";
-import { OpenAiSecretsService } from "../s
+import { OpenAiSecretsService } from "../secrets/open-ai-secrets.service";
+
+export class ConversationPromptServiceFactory {
+  private cache:
+    | { apiKey: string; 
