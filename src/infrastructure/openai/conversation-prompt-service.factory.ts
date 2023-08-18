92978a97c38a2872f2395940fb3c8a930a876dba
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-08-18 06:42:15 +0000

    yonghu id

--- src/infrastructure/openai/conversation-prompt-service.factory.ts
+++ src/infrastructure/openai/conversation-prompt-service.factory.ts
@@ -1,1 +1,3 @@
-import { ConversationPromptService } f
+import { ConversationPromptService } from "@wisegpt/gpt-conversation-prompt";
+import { Configuration, OpenAIApi } from "openai";
+import { OpenAiSecretsService } from "../s
