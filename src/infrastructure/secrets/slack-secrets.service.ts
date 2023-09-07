e143a5916babd5d284b437a134e23f269d1f74c6
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-09-07 10:41:10 +0000

    slight wording change

--- src/infrastructure/secrets/slack-secrets.service.ts
+++ src/infrastructure/secrets/slack-secrets.service.ts
@@ -10,4 +10,6 @@ type AppSecrets = {
 
 export class SlackSecretsService {
   private static readonly SLACK_SECRET_ARN = getEnv("SLACK_SECRET_ARN");
-  private static readonly APP_SE
+  private static readonly APP_SECRET_REGEX =
+    /^app\/(?<appId>[a-zA-Z0-9]+)\/(?<secretName>token|client-id|client-secret)$/;
+  private static readonly SLACK_SECRET_TTL
