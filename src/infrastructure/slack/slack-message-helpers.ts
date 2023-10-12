ab5dcfe684a0608eac1e9d80c52db4765d82771b
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-10-12 06:51:38 +0000

    add webpack

--- src/infrastructure/slack/slack-message-helpers.ts
+++ src/infrastructure/slack/slack-message-helpers.ts
@@ -1,3 +1,5 @@
 import { ChatPostMessageArguments, ChatUpdateArguments } from "@slack/web-api";
 import { ConversationEnded } from "../../domain/conversation/conversation.events";
-import { prep
+import { prepareForSlack } from "../../domain/slack-adapter/conversation-mentions";
+
+type CreateMessageOutput = Requ
