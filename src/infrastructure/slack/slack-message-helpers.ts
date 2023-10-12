ea916618e31ce10edb3bc63f9c89838bdc20d687
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-10-12 13:01:56 +0000

    update form

--- src/infrastructure/slack/slack-message-helpers.ts
+++ src/infrastructure/slack/slack-message-helpers.ts
@@ -2,4 +2,9 @@ import { ChatPostMessageArguments, ChatUpdateArguments } from "@slack/web-api";
 import { ConversationEnded } from "../../domain/conversation/conversation.events";
 import { prepareForSlack } from "../../domain/slack-adapter/conversation-mentions";
 
-type CreateMessageOutput = Requ
+type CreateMessageOutput = Required<
+  Pick<ChatPostMessageArguments, "text" | "blocks">
+>;
+
+type UpdateMessageOutput = Required<
+  Pick<ChatUpdat
