5f51847b871562e14968a63c30052bdf920cd807
DarkChocolater <125297811+DarkChocolater@users.noreply.github.com>
2023-10-29 06:42:48 +0000

    Auto publish

--- src/infrastructure/slack/slack-web-client-factory.ts
+++ src/infrastructure/slack/slack-web-client-factory.ts
@@ -1,3 +1,6 @@
 import { WebClient } from "@slack/web-api";
 import config from "../../config";
-import { SlackOAuthReaderRepository } from "../dynamodb/slack/oauth/s
+import { SlackOAuthReaderRepository } from "../dynamodb/slack/oauth/slack-oauth-reader.repository";
+import defaultSlackSecretsService, {
+  SlackSecretsService,
+} from "../secrets/slack-secrets.
